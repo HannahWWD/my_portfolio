af76a2e1f6a20e6978534cecf7b62f9614ec50c1
Hannah Ng <hannahng@hannahs-mbp-2.lan>
2020-08-26 11:11:24 -0700

    initial commit

---
+++ src/pages/home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Home(){
+
+    return (
+        <div className="container">
+            {/* <Header /> */}
+            <div className="main">
+            <h1>Hello</h1>
+
+            </div>
+            {/* <Footer /> */}
+        </div>
+
+
+    )
+}
+
+export default Home
